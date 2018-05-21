6af63f8b61b672253d00438fb3162511505d1f23
inb <m.inaba@connectom.co.jp>
2018-05-21 17:49:17 +0900

    looks better

--- main.js
+++ main.js
@@ -1,51 +1,77 @@
 const mainRegion = document.getElementById('main');
 
-let Filtering = 'all';
+const Filtering = {
+    state: 'all',
+};
 
-const Filter = {
+const FilterState = {
     view() {
         return m('div', [
             m(
                 'select',
-                { oninput: m.withAttr('value', v => Filtering = v) },
+                { oninput: m.withAttr('value', v => Filtering.state = v) },
                 ['all', 'RUNNING', 'FAILED', 'STOPPED', 'SUCCEEDED'].map(x => m('option', { value: x }, x))
             ),
         ]);
     },
 };
 
+const Running = {
+    view(vnode) {
+        return m('span', { style: { color: 'blue' }}, vnode.attrs.msg);
+    },
+};
+
+const Failed = {
+    view(vnode) {
+        return m('span', { style: { color: 'red' }}, vnode.attrs.msg);
+    },
+};
+
+const Stopped = {
+    view(vnode) {
+        return m('span', { style: { color: 'purple' }}, vnode.attrs.msg);
+    },
+};
+
+const Succeeded = {
+    view(vnode) {
+        return m('span', { style: { color: 'green' }}, vnode.attrs.msg);
+    },
+};
+
+const StateMap = {
+    RUNNING: Running,
+    FAILED: Failed,
+    STOPPED: Stopped,
+    SUCCEEDED: Succeeded,
+};
+
 const List = {
     data: [],
     conn: new WebSocket('ws://localhost:55301/ws'),
-    refetch: false,
     oninit(vnode) {
         vnode.state.conn.onmessage = ev => {
             console.log(JSON.parse(ev.data));
             vnode.state.data = JSON.parse(ev.data);
-            vnode.state.refetch = true;
+            setTimeout(() => vnode.state.conn.send('hi!'), 1000);
             m.redraw();
         };
         vnode.state.conn.onopen = ev => vnode.state.conn.send('hi!');
     },
-    onupdate(vnode) {
-        if (vnode.state.refetch) {
-            setTimeout(() => {
-                vnode.state.conn.send('hi!');
-                vnode.state.refetch = false;
-            }, 1000);
-        }
-    },
     view(vnode) {
         return m('ul', vnode.state.data.map(x => {
+            const runs = x.runs.filter(y => {
+                if (Filtering.state === 'all') return y
+                if (y.state === Filtering.state) return y
+            });
+            if (runs.length === 0) return;
             return m('li', [
                 m('div', x.job),
                 m(
                     'div',
                     {style: {'margin-left': '1em'}},
-                    x.runs.filter(y => {
-                        if (Filtering === 'all') return y
-                        if (y.state === Filtering) return y
-                    }).map(y => m('div', `${y.started_at}: ${y.state}`))
+                    runs.map(y => m('div', m(StateMap[y.state], { msg: `${y.started_at}: ${y.state}` })))
                 ),
             ])
         }))
@@ -56,7 +82,7 @@ const Component = {
     view() {
         return m(
             'div', [
-                m(Filter),
+                m(FilterState),
                 m(List)
             ]
         );
